861e5e68ec9f4e75248ef364f911dc45ab9ded39
Danny Hadley <dadleyy@gmail.com>
2017-12-11 14:14:03 -0500

    chore(hound) hound ci fixes

--- examples/library/models/multi_auto.go
+++ examples/library/models/multi_auto.go
@@ -1,5 +1,6 @@
 package models
 
+// MultiAuto represents a record w/ mutliple auto-increment directives on a postgres model.
 type MultiAuto struct {
 	table  bool   `marlow:"tableName=multi_auto&dialect=postgres&primaryKey=id"`
 	ID     uint   `marlow:"column=id&autoIncrement=true"`
